5ac6630324cfbf5d6ccda89260a2438f8642f655
rosefishh <porose404@gmail.com>
2026-07-18 12:45:52 +0530

    use of different variables and data types in java

---
+++ DAY1/variables_datatypes.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class variables_datatypes {
+    public static void main(String[] args){
+        int age = 20;
+        double gpa = 9.95;
+        char grade = 'A';
+        boolean isJavafun = true;
+        String name = "Aman";
+
+        System.out.println("Name: "+ name);
+        System.out.println("Age: "+ age);
+        System.out.println("GPA: "+ gpa);
+        System.out.println("Grade: "+ grade);
+        System.out.println("Is java fun?: "+ isJavafun);
+    }
+}
