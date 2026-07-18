4e30d04e79e3c036741d953750866fdbea96f9e9
rosefishh <porose404@gmail.com>
2026-07-18 13:39:36 +0530

    use if-else statement in java

--- DAY1/if_else.java
+++ DAY1/if_else.java
@@ -1,10 +1,25 @@
 package day1;
 
+import java.util.Scanner;
+
 public class if_else {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter your marks: ")
-        int marks = sc.nextInt
+        System.out.println("Enter your marks: ");
+        int marks = sc.nextInt();
+
+        if (marks>=90){
+            System.out.println("Grade: A ");
+        }
+        else if (marks>=75){
+            System.out.println("Grade: B ");
+        }
+        else if (marks>=50){
+            System.out.println("Grade: C ");
+        }
+    else{
+            System.out.println("FAILED!");
+        }
     }
 }
