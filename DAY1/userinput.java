0c999212e436dcf35ee861b585215643a877214e
rosefishh <porose404@gmail.com>
2026-07-18 13:02:28 +0530

    take userinput

---
+++ DAY1/userinput.java
@@ -0,0 +1,19 @@
+package day1;
+import java.util.*;
+public class userinput {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your favourite food item: ");
+        String food = sc.nextLine();
+        
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Enter your age: ");
+        String age = sc.nextLine();
+
+        System.out.println("Hello " + name + "! You are " + age + " years old and you love " + food + ".");
+    }
+    
+}
