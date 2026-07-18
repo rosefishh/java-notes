d6dc050fee3a207b5d30ca22a74dd87571c1ff79
rosefishh <porose404@gmail.com>
2026-07-18 15:44:03 +0530

    use of loops in java

---
+++ day2/loops.java
@@ -0,0 +1,26 @@
+package day2;
+
+public class loops {
+    public static void main(String[] args){
+        //for loop
+        for(int i=0;i<6;i++){
+            System.out.println(i + " ");
+        }
+
+        //while loop
+        int j=1;
+        while(j<6){
+            System.out.println(j + " ");
+            j++;
+        }
+
+        //do while loop 
+        int k =10;
+        do{
+            System.out.println(k + " ");
+            k++;
+        }while (k<= 5);
+
+    }
+    
+}
