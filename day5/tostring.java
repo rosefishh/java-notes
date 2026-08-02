fc3576bdfbc6f4f48b1bd980f95011ddfc8bb8f2
rosefishh <porose404@gmail.com>
2026-08-02 16:19:25 +0530

    using toString method in java

--- day5/tostring.java
+++ day5/tostring.java
@@ -1,18 +1,23 @@
 package day5;
 
 public class tostring {
-    
+    public static void main(String[] args){
+        car2 c1 = new car2("mustang",7);
+        System.out.println(c1);
+    }
 }
 
-class car{
+class car2{
     String name;
     int number;
 
-    car(String name,int number){
+    car2(String name,int number){
         this.name = name;
         this.number = number;
     }
 
     @Override
-    
+    public String toString(){
+        return "car2{name='"+this.name+ "' , number ="+this.number+ "}";
+    }
 }
