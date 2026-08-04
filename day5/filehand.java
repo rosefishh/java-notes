1dda5a6c9e5084da0b8380adcdcffc1e91b8ff1e
rosefishh <porose404@gmail.com>
2026-08-04 23:53:42 +0530

    file handling in java

---
+++ day5/filehand.java
@@ -0,0 +1,24 @@
+package day5;
+import java.io.*;
+
+public class filehand {
+    public static void main(String[] args) {
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader("C:\\javarev\\day5\\input.txt"));
+
+            PrintWriter writer = new PrintWriter("C:\\javarev\\day5\\output.txt");
+
+            String line;
+            while((line = reader.readLine())!=null){
+                writer.println(line);
+            }
+            reader.close();
+            writer.close();
+            System.out.println("file copied sucessfuly!");
+
+        }
+            catch(Exception e){
+                System.out.println("ERROR");
+            }
+        }
+}
