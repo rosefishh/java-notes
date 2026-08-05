e659319164d2b913718e16744e19acbd3383cb46
rosefishh <porose404@gmail.com>
2026-08-05 18:45:30 +0530

    use of timer task in java

---
+++ day5/timertask.java
@@ -0,0 +1,23 @@
+package day5;
+
+import java.util.*;
+
+public class timertask {
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask(){
+            int count = 5;
+            @Override
+            public void run(){
+                System.out.println("TIME TO SLEEP");
+                count--;
+                if(count<=0){
+                    timer.cancel();
+                }
+            }        
+        };
+        timer.schedule(task,0,1000);
+    }
+}
+
+
